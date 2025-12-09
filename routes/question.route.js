5d25cd13000aa90398d5749ed1502cf45c27eced
Ushio12345 <anhqueht1100@gmail.com>
2025-12-09 21:51:33 +0700

    push swagger ui

--- routes/question.route.js
+++ routes/question.route.js
@@ -6,6 +6,39 @@ const {
   verifyAdmin,
   verifyAuthor,
 } = require("../middlewares/authenticate");
+
+/**
+ * @swagger
+ * tags:
+ *   name: Questions
+ *   description: Question management endpoints
+ */
+
+/**
+ * @swagger
+ * /api/questions:
+ *   get:
+ *     summary: Get all questions
+ *     tags: [Questions]
+ *     security:
+ *       - cookieAuth: []
+ *     responses:
+ *       200:
+ *         description: List of questions
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Question'
+ *       401:
+ *         description: Unauthorized
+ */
 QuestionRoute.get(
   "/",
   verifyUser,
@@ -13,6 +46,26 @@ QuestionRoute.get(
   questionController.getAllQuestion
 );
 
+/**
+ * @swagger
+ * /api/questions:
+ *   post:
+ *     summary: Create a new question
+ *     tags: [Questions]
+ *     security:
+ *       - cookieAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Question'
+ *     responses:
+ *       201:
+ *         description: Question created successfully
+ *       400:
+ *         description: Bad request
+ */
 QuestionRoute.post(
   "/",
   verifyUser,
@@ -20,24 +73,135 @@ QuestionRoute.post(
   questionController.createQuestion
 );
 
+/**
+ * @swagger
+ * /api/questions/{questionId}:
+ *   get:
+ *     summary: Get a question by ID
+ *     tags: [Questions]
+ *     parameters:
+ *       - in: path
+ *         name: questionId
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The question ID
+ *     responses:
+ *       200:
+ *         description: Question details
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Question'
+ *       404:
+ *         description: Question not found
+ */
+QuestionRoute.get("/:questionId", questionController.getQuestionById);
+
+/**
+ * @swagger
+ * /api/questions/{questionId}:
+ *   put:
+ *     summary: Update a question
+ *     tags: [Questions]
+ *     security:
+ *       - cookieAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: questionId
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The question ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Question'
+ *     responses:
+ *       200:
+ *         description: Question updated
+ *       400:
+ *         description: Bad request
+ *       403:
+ *         description: Forbidden
+ *       404:
+ *         description: Question not found
+ */
 QuestionRoute.put(
   "/:questionId",
   verifyUser,
   verifyAuthor,
   questionController.updateQuestion
 );
+
+/**
+ * @swagger
+ * /api/questions/{questionId}:
+ *   delete:
+ *     summary: Delete a question
+ *     tags: [Questions]
+ *     security:
+ *       - cookieAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: questionId
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The question ID
+ *     responses:
+ *       200:
+ *         description: Question deleted
+ *       403:
+ *         description: Forbidden
+ *       404:
+ *         description: Question not found
+ */
 QuestionRoute.delete(
   "/:questionId",
   verifyUser,
   verifyAuthor,
   questionController.deleteQuestion
 );
-QuestionRoute.get(
-  "/:questionId",
-
-  questionController.getQuestionById
-);
-
-// router.delete("/:questionId", questionController.deleteQuestion);
 
 module.exports = QuestionRoute;
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Question:
+ *       type: object
+ *       required:
+ *         - text
+ *         - options
+ *         - correctAnswerIndex
+ *         - author
+ *       properties:
+ *         text:
+ *           type: string
+ *           description: The question text
+ *         options:
+ *           type: array
+ *           items:
+ *             type: string
+ *           description: Array of options (at least 4)
+ *         keywords:
+ *           type: array
+ *           items:
+ *             type: string
+ *           description: Array of keywords
+ *         correctAnswerIndex:
+ *           type: integer
+ *           description: Index of the correct option
+ *         quiz:
+ *           type: array
+ *           items:
+ *             type: string
+ *           description: Related quiz IDs
+ *         author:
+ *           type: string
+ *           description: Author user ID
+ */
--- routes/quizz.route.js
+++ routes/quizz.route.js
@@ -3,18 +3,164 @@ const QuizzRoute = express.Router();
 const quizzesController = require("../controllers/quiz.controller");
 const { verifyUser, verifyAdmin } = require("../middlewares/authenticate");
 
-QuizzRoute.route("/")
-  .get(quizzesController.getAllQuizzes)
-  .post(verifyUser, verifyAdmin, quizzesController.createQuiz);
+/**
+ * @swagger
+ * tags:
+ *   name: Quizzes
+ *   description: Quiz management endpoints
+ */
 
-QuizzRoute.route("/:id")
+/**
+ * @swagger
+ * /api/quizzes:
+ *   get:
+ *     summary: Get all quizzes
+ *     tags: [Quizzes]
+ *     responses:
+ *       200:
+ *         description: List of all quizzes
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Quiz'
+ */
+QuizzRoute.get("/", quizzesController.getAllQuizzes);
 
-  .get(quizzesController.getDetailQuiz)
+/**
+ * @swagger
+ * /api/quizzes:
+ *   post:
+ *     summary: Create a new quiz
+ *     tags: [Quizzes]
+ *     security:
+ *       - cookieAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Quiz'
+ *     responses:
+ *       201:
+ *         description: Quiz created successfully
+ *       400:
+ *         description: Invalid input
+ */
+QuizzRoute.post("/", verifyUser, verifyAdmin, quizzesController.createQuiz);
 
-  .put(verifyUser, verifyAdmin, quizzesController.editQuiz)
-  // DELETE quiz
-  .delete(verifyUser, verifyAdmin, quizzesController.deleteQuiz);
+/**
+ * @swagger
+ * /api/quizzes/{id}:
+ *   get:
+ *     summary: Get quiz detail by ID
+ *     tags: [Quizzes]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Quiz ID
+ *     responses:
+ *       200:
+ *         description: Quiz detail
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Quiz'
+ *       404:
+ *         description: Quiz not found
+ */
+QuizzRoute.get("/:id", quizzesController.getDetailQuiz);
 
+/**
+ * @swagger
+ * /api/quizzes/{id}:
+ *   put:
+ *     summary: Update a quiz
+ *     tags: [Quizzes]
+ *     security:
+ *       - cookieAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Quiz'
+ *     responses:
+ *       200:
+ *         description: Quiz updated successfully
+ *       404:
+ *         description: Quiz not found
+ */
+QuizzRoute.put("/:id", verifyUser, verifyAdmin, quizzesController.editQuiz);
+
+/**
+ * @swagger
+ * /api/quizzes/{id}:
+ *   delete:
+ *     summary: Delete a quiz
+ *     tags: [Quizzes]
+ *     security:
+ *       - cookieAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Quiz deleted successfully
+ *       404:
+ *         description: Quiz not found
+ */
+QuizzRoute.delete(
+  "/:id",
+  verifyUser,
+  verifyAdmin,
+  quizzesController.deleteQuiz
+);
+
+/**
+ * @swagger
+ * /api/quizzes/{id}/question:
+ *   post:
+ *     summary: Add a single question to a quiz
+ *     tags: [Quizzes]
+ *     security:
+ *       - cookieAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Quiz ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               questionId:
+ *                 type: string
+ *                 description: Question ID
+ *     responses:
+ *       200:
+ *         description: Question added to quiz
+ *       404:
+ *         description: Quiz not found
+ */
 QuizzRoute.post(
   "/:id/question",
   verifyUser,
@@ -22,12 +168,63 @@ QuizzRoute.post(
   quizzesController.addNewQuestion
 );
 
+/**
+ * @swagger
+ * /api/quizzes/{id}/questions:
+ *   post:
+ *     summary: Add multiple questions to a quiz
+ *     tags: [Quizzes]
+ *     security:
+ *       - cookieAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Quiz ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               questionIds:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *     responses:
+ *       200:
+ *         description: Questions added to quiz
+ *       404:
+ *         description: Quiz not found
+ */
 QuizzRoute.post(
   "/:id/questions",
   verifyUser,
   verifyAdmin,
-
   quizzesController.addMultipleQuestions
 );
 
 module.exports = QuizzRoute;
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Quiz:
+ *       type: object
+ *       required:
+ *         - title
+ *         - description
+ *       properties:
+ *         title:
+ *           type: string
+ *         description:
+ *           type: string
+ *         questions:
+ *           type: array
+ *           items:
+ *             type: string
+ */
